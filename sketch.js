960ecd5fc90aac06a7990e9496c3bea03d0189b3
jojohnathon <pixelizedjohnny@gmail.com>
2024-04-24 01:50:36 -0700

    implement isometric view

--- sketch.js
+++ sketch.js
@@ -1,21 +1,33 @@
 let shared;
 let textures;
 
+// These are the four numbers that define the transform, i hat and j hat
+const i_x = 1;
+const i_y = 0.5;
+const j_x = -1;
+const j_y = 0.5;
+
+// Sprite size
+const w = 256;
+const h = 256;
+
+
+
 function preload() {
-  textures = loadImage('./assets/tinyBlocks.png')
-	partyConnect(
+  partyConnect(
     "wss://demoserver.p5party.org",
     "app-name"
 	);
   shared = partyLoadShared("shared", { x: 100, y: 100 });
+
+  textures = loadImage('./assets/tile_022.png')
 }
 
 function setup() {
-  createCanvas(400, 400);
-  noStroke();
-  console.log(width)
-  test = createImage(18, 18)
-  test.copy(textures, 0, 0, 18, 18, 0, 0, 18, 18)
+  createCanvas(1920, 1080);
+  // TODO create fullscreen button
+
+  textures.resize(w, h)
 }
 
 function mousePressed() {
@@ -23,10 +35,33 @@ function mousePressed() {
   shared.y = mouseY;
 }
 
+
 function draw() {
-  background("#ffcccc");
-  fill("#000066");
-  
-  ellipse(shared.x, shared.y, 100, 100);
-  image(test, 0, 0, 18 * 4, 18 * 4) 
+  background(255);
+  translate(width / 2, height / 4);
+
+  // ellipse(shared.x, shared.y, 100, 100);
+
+
+  for(let i = 0; i < 10; i++){
+    for(let j = 0; j < 10; j++){
+      let tsc = to_screen_coordinate({x: i, y: j});
+      image(textures, tsc.x, tsc.y)
+    }
+  }
+}
+
+
+function to_screen_coordinate(tile) {
+  // Without accounting for sprite size
+  // return {
+  //   x: tile.x * i_x + tile.y * j_x,
+  //   y: tile.x * i_y + tile.y * j_y,
+  // }
+
+  // Accounting for sprite size
+  return {
+    x: tile.x * i_x * 0.5 * w + tile.y * j_x * 0.5 * w,
+    y: tile.x * i_y * 0.5 * h + tile.y * j_y * 0.5 * h,
+  }
 }
